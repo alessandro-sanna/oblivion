44ad7e0c844e32ccb799d7705a2ade936eca697a
Alessandro <sanna.alessandro.1996@gmail.com>
2022-03-30 10:14:40 +0200

    prosecuzione della scrittura dei metodi di testing per officesandbox

--- .OblivionTest/.test_office_sandboxing.py
+++ .OblivionTest/.test_office_sandboxing.py
@@ -1,5 +1,8 @@
 import os.path
 import unittest
+
+import pywintypes
+
 from OblivionSource.FileExecutionPhase import OfficeSandbox
 import psutil
 
@@ -7,14 +10,12 @@ import psutil
 class MyTestCase(unittest.TestCase):
     def __init__(self):
         super().__init__()
-        self.TestClass = OfficeSandbox()
+
         self.app = None
+        self.program = "word"
+        self.office_file = None
 
-    def test_build_strings(self):
-        pass
-
-    def test_get_instrumented_macros(self):
-        pass
+        self.TestClass = OfficeSandbox()
 
     def test_open_program(self):
         app = self.TestClass.__open_program()
@@ -25,39 +26,56 @@ class MyTestCase(unittest.TestCase):
         self.app = app
 
     def test_open_file(self):
-        doc = self.TestClass.__open_file(self.TestClass.program, self.app, self.TestClass.running_file)
+        doc = self.TestClass.__open_file(self.program, self.app, self.TestClass.running_file)
         file_basename = os.path.basename(self.TestClass.running_file)
-        if "word" == self.TestClass.program:
-            active_file = self.app.ActiveDocument.Name
-        elif "excel" == self.TestClass.program:
-            active_file = self.app.ActiveWorkbook.Name
-        else:
-            active_file = None
-        doc_name = doc.Name
+        try:
+            if "word" == self.program:
+                active_file = self.app.ActiveDocument.Name
+            elif "excel" == self.program:
+                active_file = self.app.ActiveWorkbook.Name
+            else:
+                active_file = None
+        except pywintypes.com_error as exc:
+            self.assertNotEqual(exc.excepinfo[-2], 37016)
+            # questo codice di errore significa che il documento non è stato aperto
 
+        doc_name = doc.Name
         self.assertEqual(file_basename, active_file)
         self.assertEqual(active_file, doc_name)
+        self.office_file = doc
 
-    def test_replace_macros(self, office_file):
-        pass
+    def test_empty_macros(self):
+        self.TestClass.__empty_macros(self.office_file)
+        for macro in self.office_file.VBProject.VBComponents:
+            self.assertEqual(macro.CodeModule.CountOfLines, 0)
 
-    @staticmethod
-    def test_empty_macros(office_file):
-        pass
+    def test_make_macros(self):
+        self.TestClass.__get_instrumented_macros()
+        doc = self.TestClass.__make_macros(self.office_file)
+        macro_dict = self.TestClass.macro_dict
+        for macro in doc.VBProject.VBComponents:
+            name = macro.Name
+            try:
+                ext = self.TestClass.macro_extension_code_dict_rev[macro.Type]
+            except KeyError:
+                ext = "cls"
+            key = name + '.' + ext
+            self.assertIn(key, macro_dict.keys())
 
-    def test_make_macros(self, office_file):
-        pass
-
-    def test_write_macros(self, office_file):
-        pass
-
-    @staticmethod
-    def test_close_file(office_file):
-        pass
-
-    @staticmethod
-    def test_close_program(app):
-        pass
+    def test_write_macros(self):
+        self.TestClass.__get_instrumented_macros()
+        doc = self.TestClass.__write_macros(self.office_file)
+        macro_dict = self.TestClass.macro_dict
+        for macro in doc.VBProject.VBComponents:
+            name = macro.Name
+            try:
+                ext = self.TestClass.macro_extension_code_dict_rev[macro.Type]
+            except KeyError:
+                ext = "cls"
+            key = name + '.' + ext
+            target_code = macro_dict[key]
+            this_code = macro.CodeModule.Lines(1, macro.CodeModule.CountOfLines)
+            self.assertEqual(this_code, target_code)
 
 
 if __name__ == '__main__':
